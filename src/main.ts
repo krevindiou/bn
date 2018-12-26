a97f3e555d16d1eba5275e34e79be56fbe2f3a9a
krevindiou <krevindiou@users.noreply.github.com>
2018-12-26 19:23:19 +0100

    Create Swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle('BN API')
+        .setVersion('0.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('specs', app, document);
+
     await app.listen(8080);
 }
 bootstrap();
