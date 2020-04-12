66bce0bec1ccddc1172df1e672ac52e2f523f249
krevindiou <krevindiou@users.noreply.github.com>
2020-04-12 12:22:28 +0200

    Validate env vars

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MassiveModule, MassiveConnectOptions } from '@nestjsplus/massive';
 import { ConfigModule, ConfigType } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 import AppController from './app.controller';
 import AuthModule from './auth/auth.module';
 import MemberModule from './member/member.module';
@@ -9,7 +10,16 @@ import DatabaseConfig from './config/database.config';
 @Module({
     controllers: [AppController],
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                JWT_SECRET_KEY: Joi.string().required(),
+                DATABASE_USER: Joi.string().required(),
+                DATABASE_PASSWORD: Joi.string().required(),
+                DATABASE_HOST: Joi.string().required(),
+                DATABASE_PORT: Joi.number().default(5432),
+                DATABASE_NAME: Joi.string().required(),
+            }),
+        }),
         MassiveModule.registerAsync({
             imports: [ConfigModule.forFeature(DatabaseConfig)],
             useFactory: async (config: ConfigType<typeof DatabaseConfig>) =>
