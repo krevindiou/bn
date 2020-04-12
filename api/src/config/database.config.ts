189fdb1d0e0ea2f9177cbe6180eaa4aafbe9e199
krevindiou <krevindiou@users.noreply.github.com>
2020-04-12 15:49:43 +0200

    Fix linter

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -4,6 +4,6 @@ export default registerAs('database', () => ({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT!, 10),
+    port: parseInt(process.env.POSTGRES_PORT || '', 10),
     database: process.env.POSTGRES_DB,
 }));
